a61cdd7a5c5fe39cd7017d948b569f4afab42cfe
Santiago Triana <a.trianaa@uniandes.edu.co>
2022-09-28 17:41:35 -0500

    Añadir archivos .py

---
+++ lectura.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+continue_reading = True
+
+def end_read(signal, frame):
+    global continue_reading
+    print("Ctrl´´ captured, ending read.")
+    continue_reading = False
+    GPIO.cleanup()
+
+signal.signal(signal.SIGINT, end_read)
+#Crear el objeto de lectura
+
+
+def readRFID():
+    print("Acerque la tarjeta al lector")
+    id, text = reader.read()
+    print(text)
+
+menu = "Menú de opciones\n\t1.Leer registros.\nIngrese la opción que desesa ejecutar (ej:4\n"
+
+def showTerminal():
+    while True:
+        option = input(menu)
+        if option == "1":
+            print("Leyendo registro")
+            readRFID()
+        else:
+            print("Opción no válida")
+
+showTerminal()
